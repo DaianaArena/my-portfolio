881c855976eaf3605cd6a97ee0e3f7f64760a005
DaianaArena <84255114+DaianaArena@users.noreply.github.com>
2022-03-29 00:29:41 -0300

    mobile version testing

---
+++ js/navbar.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burguer = document.querySelector('.burguer');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burguer.addEventListener('click', () => {
+
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade  2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both ${index / 7}s`;
+            }
+        });
+
+        //animate burger
+        burguer.classList.toggle('burguer-active');
+
+    });
+
+
+}
+navSlide();
